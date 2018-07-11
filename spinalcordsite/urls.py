7935ea124ecc58528823a7692dff0b647714ebfe
spinalcord <spinal@spinal.com>
2018-07-11 04:16:07 -0700

    My Spinal Cored Correction system app, first commit

---
+++ spinalcordsite/urls.py
@@ -0,0 +1,25 @@
+"""spinalcordsite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+  path('admin/', admin.site.urls),
+]
+
+admin.site.site_header = "SPINAL CORD CORRECTON SYSTEM "
+admin.site.site_title = "SPINAL CORD CORRECTON SYSTEM ADMIN PORTAL"
+admin.site.index_title = "WELCOME TO SPINAL CORD CORRECTON SYSTEM PORTAL"
